c05699a08f1fbfd8f2979d71f01bdb39f9450c10
MaksimFurgala <furgala.maxim@yandex.ru>
2024-10-26 15:37:39 +0300

    Add project files.

---
+++ Notes.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notes.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => 
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        internal Guid UserId => !User.Identity.IsAuthenticated
+            ? Guid.Empty
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
